858b1507de32f35b2ca356208cd14fe06579cb09
Egon Willighagen <egon.willighagen@gmail.com>
2012-08-28 08:39:49 +0200

    Fixed the lib load script name

--- pvxmlrpc/R/pvxmlrpc.R
+++ pvxmlrpc/R/pvxmlrpc.R
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-.packageName <- "rrdf"
+.packageName <- "pvxmlrpc"
 
 #require(rJava, quietly=TRUE)
 #require(pvxmlrpclibs, quietly=TRUE)
