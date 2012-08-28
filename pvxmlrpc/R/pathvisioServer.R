2073247e51475c933b6b092a95bc2930fb27ea6a
Egon Willighagen <egon.willighagen@gmail.com>
2012-08-28 21:19:06 +0200

    Return the port, instead of the full server URL

--- pvxmlrpc/R/pathvisioServer.R
+++ pvxmlrpc/R/pathvisioServer.R
@@ -1,5 +1,6 @@
 pathvisioServer <- function(port=9000) {
   server = .jnew("org/pathvisio/pathvisiorpc/RpcServer")
   port = .jcall(server, "I", "startServer", as.integer(port))
-  paste("http://localhost:", port, "/", sep="") 
+  #paste("http://localhost:", port, "/", sep="") 
+  port
 }
