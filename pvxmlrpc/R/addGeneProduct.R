6633882313ca1c824e13e52a3f5cf0392c14529d
christlee <christleemans@gmail.com>
2012-11-13 16:39:19 +0000

    addGeneProduct method created
    Conflicts:
    	pvxmlrpc/NAMESPACE

---
+++ pvxmlrpc/R/addGeneProduct.R
@@ -0,0 +1,6 @@
+addGeneProduct <- function(pathway, name, host="localhost", port=9000) {
+  if (missing(pathway)) stop("You must provide a pathway name.");
+  if (missing(name)) stop("You must provide a gene product name.");
+
+  addDataNode(pathway, name, "GeneProduct", host, port)
+}
