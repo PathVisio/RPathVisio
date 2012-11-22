4ee3fcf73a19202d23f599606b29f22d9625b425
christlee <christleemans@gmail.com>
2012-11-22 14:30:21 +0000

    changed names of specific add function to have same convention as addPathwayNode

--- pvxmlrpc/R/addMetabolite.R
+++
@@ -1,6 +0,0 @@
-addMetabolite <- function(pathway, name, host="localhost", port=9000) {
-  if (missing(pathway)) stop("You must provide a pathway name.");
-  if (missing(name)) stop("You must provide a metabolite name.");
-
-  addDataNode(pathway, name, "Metabolite", host, port)
-}
