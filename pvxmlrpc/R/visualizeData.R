49f838e6c1b41e9c21f295b1b43e6d4c7064843f
christlee <christleemans@gmail.com>
2012-11-22 14:44:51 +0000

    changed 3 small typo's

--- pvxmlrpc/R/visualizeData.R
+++ pvxmlrpc/R/visualizeData.R
@@ -1,4 +1,4 @@
-visualizeData <- function(pathway, gexname, dbname)  host="localhost", port=9000) {
+visualizeData <- function(pathway, gexname, dbname, host="localhost", port=9000) {
   if (missing(pathway)) stop("You must provide a pathway name.");
   if (missing(gexname)) stop("You must provide the name of the gexfile to use.");
   if (missing(dbname)) stop("You must provide the name of the database to use for mapping the data.");
