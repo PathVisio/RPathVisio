6bee1ee2a4420681c3fc39a9e1aec58707ef7cbf
Anwesha <pennatula@gmail.com>
2012-11-26 13:08:27 +0100

    Update pvxmlrpc/R/createPathway.R

--- pvxmlrpc/R/createPathway.R
+++ pvxmlrpc/R/createPathway.R
@@ -1,6 +1,6 @@
-createPathway <- function(name, host="localhost", port=9000, author="Anonymous") {
+createPathway <- function(name, host="localhost", port=9000, author="Anonymous", organism="") {
   if (missing(name)) stop("You must provide a pathway name.");
 
   hostUrl = paste("http://", host, ":", port, "/", sep="")
-  xml.rpc(hostUrl, "PathwayHandler.createPathway", author, name)
+  xml.rpc(hostUrl, "PathVisio.createPathway", author, name, organism)
 }
