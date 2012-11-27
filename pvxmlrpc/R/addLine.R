c3d8201067f5273e6cd0c150caa27a013262bf51
Egon Willighagen <egon.willighagen@gmail.com>
2012-11-27 13:11:18 +0100

    Fixed syntax issues

--- pvxmlrpc/R/addLine.R
+++ pvxmlrpc/R/addLine.R
@@ -1,9 +1,9 @@
-addLine <- function(pathway, name, startNode, endNode, startAnnotation="", endAnnotation="Arrow", lineID=" ", linesource=" " host="localhost", port=9000) {
+addLine <- function(pathway, name, startNode, endNode, startAnnotation="", endAnnotation="Arrow", lineID=" ", linesource=" ", host="localhost", port=9000) {
   if (missing(pathway)) stop("You must provide a pathway name.");
   if (missing(name)) stop("You must provide a line name.");
   if (missing(startNode)) stop("You must provide a start node.");
   if (missing(endNode)) stop("You must provide a end node.");
 
   hostUrl = paste("http://", host, ":", port, "/", sep="")
-  xml.rpc(hostUrl, "PathVisio.addLine", pathway, name, startNode, endNode, startAnnotation, endAnnotation, lineID. linesource)
+  xml.rpc(hostUrl, "PathVisio.addLine", pathway, name, startNode, endNode, startAnnotation, endAnnotation, lineID, linesource)
 }
