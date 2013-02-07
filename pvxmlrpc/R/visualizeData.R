bda49924259c4e3a59614568e172c78fdb53aaa8
christlee <christleemans@gmail.com>
2013-02-07 12:36:32 +0000

    changed standard wd into hd + /PathVisioRPC-Results

--- pvxmlrpc/R/visualizeData.R
+++ pvxmlrpc/R/visualizeData.R
@@ -2,11 +2,11 @@ visualizeData <- function(pathway, gexname, dbname, host="localhost", port=9000,
   if (missing(pathway)) stop("You must provide a pathway name.");
   if (missing(gexname)) stop("You must provide the name of the gexfile to use.");
   if (missing(dbname)) stop("You must provide the name of the database to use for mapping the data.");
-  if (is.na(pwypath)) pwypath = getwd();
-  if (is.na(gexpath)) gexpath = getwd();
-  if (is.na(dbpath)) dbpath = getwd();
-  if (is.na(unlist(strsplit(gexname,"\\."))[2])) gexname = paste(gexname,".pgex",sep="")
-  if (is.na(unlist(strsplit(dbname,"\\."))[2])) dbname = paste(dbname,".bridge",sep="")
+  if (is.na(pwypath)) pwypath = paste(path.expand("~"),"/PathVisioRPC-Results",sep="");
+  if (is.na(gexpath)) gexpath = paste(path.expand("~"),"/PathVisioRPC-Results",sep="");
+  if (is.na(dbpath)) dbpath = paste(path.expand("~"),"/PathVisioRPC-Results",sep="");
+  if (is.na(unlist(strsplit(gexname,"\\."))[2])) gexname = paste(gexname,".pgex",sep="");
+  if (is.na(unlist(strsplit(dbname,"\\."))[2])) dbname = paste(dbname,".bridge",sep="");
  
   pwy = paste(pwypath,"/",pathway,".gpml",sep="")
   gex = paste(gexpath,"/",gexname,sep="")
