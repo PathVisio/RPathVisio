ed665776b9c671557f14ca8bd43b8af2f7a5ac07
christlee <christleemans@gmail.com>
2013-02-15 17:27:49 +0000

    problem with outputdir

--- pvxmlrpc/R/visualizeDataByURI.R
+++ pvxmlrpc/R/visualizeDataByURI.R
@@ -1,9 +1,10 @@
-visualizeDataByURI <- function(uri, gexname, dbname, host="localhost", port=9000, pwypath=NA, gexpath=NA, dbpath=NA, outputdir="") {
+visualizeDataByURI <- function(uri, gexname, dbname, host="localhost", port=9000, pwypath=NA, gexpath=NA, dbpath=NA, outputdir=NA) {
   if (missing(uri)) stop("You must provide WikiPathway ID for the pathway");
   if (missing(gexname)) stop("You must provide the name of the gexfile to use.");
   if (missing(dbname)) stop("You must provide the name of the database to use for mapping the data.");
   if (is.na(gexpath)) gexpath = paste(path.expand("~"),"/PathVisioRPC-Results",sep="");
   if (is.na(dbpath)) dbpath = paste(path.expand("~"),"/PathVisioRPC-Results",sep="");
+  if (is.na(outputdir)) outputdir = paste(path.expand("~"),"/PathVisioRPC-Results",sep="");
   if (is.na(unlist(strsplit(gexname,"\\."))[2])) gexname = paste(gexname,".pgex",sep="");
   if (is.na(unlist(strsplit(dbname,"\\."))[2])) dbname = paste(dbname,".bridge",sep="");
  
