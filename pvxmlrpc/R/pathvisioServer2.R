222d5c16295d720ec558eda0b2762fbc4565e954
christlee <christleemans@gmail.com>
2013-04-25 16:55:23 +0100

    workaround for derby problem

--- pvxmlrpc/R/pathvisioServer2.R
+++ pvxmlrpc/R/pathvisioServer2.R
@@ -1,7 +1,4 @@
-pathvisioServer <- function(port=9000) {
-  ##work around, otherwise the driver can't be found.
-  .jnew("org/apache/derby/jdbc/EmbeddedDriver")
-
+pathvisioServer2 <- function(port=9000) {
   server = .jnew("org/pathvisio/xmlrpc/JavaServer")
   start = .jcall(server, "V", "main", c(port,""))
   paste("http://localhost:", port, "/", sep="") 
