711b009f366164fd6850a9ba82a96f1eaacb0773
christlee <christleemans@gmail.com>
2013-04-25 16:56:45 +0100

    accidently added other file

--- pvxmlrpc/R/pathvisioServer2.R
+++
@@ -1,6 +0,0 @@
-pathvisioServer2 <- function(port=9000) {
-  server = .jnew("org/pathvisio/xmlrpc/JavaServer")
-  start = .jcall(server, "V", "main", c(port,""))
-  paste("http://localhost:", port, "/", sep="") 
-  port
-}
